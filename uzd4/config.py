cb77ec2987e9b1605c3b51ae35a2b88510ba40be
Rytis <rytis.stankus@gmail.com>
2015-05-14 16:04:24 +0300

    Fixed Runge-Kutta midpoint method

--- uzd4/config.py
+++ uzd4/config.py
@@ -1,14 +1,14 @@
 import math
 
 init_N = 1
-end_N = 256
+end_N = 64
 function = lambda x: (math.e ** x - 1) / (math.e ** x + 1)
 a = 1
 b = 2
 
 function2 = lambda x, u: math.cos(x+u) + 1.5*(x-u)
-max_h = 1
-min_h = 0.0001
+max_h = 0.125
+min_h = 0.001
 u0 = 1
 start = 0
 end = 1
--- uzd4/main.py
+++ uzd4/main.py
@@ -64,26 +64,6 @@ print("{:-<80}".format(""))
 
 h = max_h
 
-# function2 = lambda x, u: -u + math.sin(x)
-# h = 0.05*math.pi
-# # u0 = 1
-# start = 0
-# end = math.pi
-
-# h = float(input("Įveskite žingsnio dydį: "))
-# u0 = float(input("Įveskite pradinę reikšmę: "))
-
-# result = runge_kutta_midpoint(function2, start, end, h, u0)
-# result2 = runge_kutta_midpoint(function2, start, end, h*2, u0)
-
-# print("{:^9} | {:^19} | {:^19} | {:^19} | {:^19} |".format("Step", "x", "u su h", "u su 2h", "Paklaida"))
-# print("{:-<99}".format(""))
-# for i in range(0, len(result)):
-#     if i % 2 == 0:
-#         print("{:>9d} | {:>19.12f} | {:>19.12f} | {:>19.12f} | {:>19.12f} |".format(i, result[i][0], result[i][1],result2[int(i/2)][1], runge_error(result2[int(i/2)][1], result[i][1], 2)))
-#     else:
-#         print("{:>9d} | {:>19.12f} | {:>19.12f} | {:>19} | {:>19} |".format(i, result[i][0], result[i][1], "", ""))
-
 last_result = runge_kutta_midpoint(function2, start, end, h, u0)
 last_error = -1
 while h >= min_h:
@@ -100,16 +80,6 @@ while h >= min_h:
     last_result = new_result
     last_error = error
 
-
-
-
-# x, y = zip(*result)
-# x2, y2 = zip(*result2)
-#
-#
-# plt.plot(x, y, 'b')
-# plt.plot(x2, y2, 'r')
-# plt.legend(["h = {}".format(h), "h = {}".format(h*2)])
 plt.xlabel("x")
 plt.ylabel("u")
 
